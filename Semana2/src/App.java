8658e72ca20bd45300beaf2db2a79d9bc7ba6837
ricardobad <ricardo.badilla@uamcr.net>
2021-09-22 20:12:58 -0600

    testt

---
+++ Semana2/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int var =1;
+        Persona estudiante = new Persona();
+        estudiante.id=1;
+        estudiante.nombre= "John";
+        estudiante.apellido= "Doe";
+        estudiante.edad = 25;
+        estudiante.saludar();
+
+    }
+}
---
+++ Semana2/src/Persona.java
@@ -0,0 +1,14 @@
+public class Persona {
+   int id;
+   String nombre;
+   String apellido;
+   short edad; 
+
+   void saludar(){
+System.out.println("Hola me llamo" + nombre);
+
+
+
+
+   }
+}
